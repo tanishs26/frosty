c49ac4ce183e6cd831c764b7daf8058b01e39e7d
tanishs26 <tanishsharma2627@gmail.com>
2025-03-03 18:56:33 +0530

    basic file setup -installing react -installing node modules -making base files

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css'; // or wherever your Tailwind CSS is imported
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <App />
+ 
+);
