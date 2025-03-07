6fd03f1911cf961ece3d7b94bf66cad85bbd2621
tanishs26 <tanishsharma2627@gmail.com>
2025-03-07 23:04:32 +0530

    changed responsiveness of trending section

--- src/App.jsx
+++ src/App.jsx
@@ -74,7 +74,6 @@ const App = () => {
       <div className="pattern" />
       <div className="wrapper">
         <header>
-          <img src="./hero.png" alt="Hero Banner" className="kit-img" />
           <h1>
             Your Favorite <span className="text-gradient">Movies </span>
             without any hassle with{" "}
@@ -111,36 +110,7 @@ const App = () => {
         </section>
       </div>
 
-      <footer className="  bottom-0 left-0 z-20 w-full p-4 bg-white border-t border-gray-200 shadow-sm md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 dark:border-gray-600">
-        <span className="text-sm text-gray-500 sm:text-center dark:text-indigo-400">
-          © 2023{"  "}
-          <a
-            href="https://github.com/tanishs26"
-            target="_blank"
-            className="hover:underline"
-          >
-            tanishs26
-          </a>
-        </span>
-        <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-indigo-500 dark:text-indigo-400 sm:mt-0">
-          <li>
-            <a href="#" className="hover:underline me-4 md:me-6">
-              About
-            </a>
-          </li>
-
-          <li>
-            <a href="#" className="hover:underline me-4 md:me-6">
-              Licensing
-            </a>
-          </li>
-          <li>
-            <a href="#" className="hover:underline">
-              Contact
-            </a>
-          </li>
-        </ul>
-      </footer>
+     
     </main>
   );
 };
